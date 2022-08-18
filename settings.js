5c9ffa04235949aa9ed3c5980eaf35891c66d375
botsenal <111542088+botsenal@users.noreply.github.com>
2022-08-19 04:54:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94765400443']
+global.premium = ['94765400443']
+global.ownernomer = '94765400443'
+global.ownername = '🐦Senal Boy'
+global.botname = 'sudu-menika'
+global.footer = '©SenalBoyInc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
+global.region = 'srilanka, South rathnapura, ehaliyagoda'
+global.sc = 'https://github.com/botsenal/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'sudu-menika'
+global.author = '🐦Senal Boy'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
